9b1f4077bc6b67f96b2351c17969e41c1e8f1bd6
ALUNO <ALUNO@LABFPC22>
2024-02-28 12:12:28 -0300

    Adicionar arquivos de projeto.

---
+++ lista_ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista_ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, soma;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+
+            soma = a + b;
+            Console.WriteLine("SOMA = " + soma);
+            Console.ReadKey();
+
+        }
+    }
+}
